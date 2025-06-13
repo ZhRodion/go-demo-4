9e45d6419e7ec5b2d96557f9bc2dce27b084c19f
Rodion Zherdev <r.zherdev@vigbo.com>
2025-06-13 15:01:17 +0400

    feat: Добавялет функцию разворота массива

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	a := [4]int{1, 2, 3, 4}
+	reverse(&a)
+	fmt.Println(a)
+}
+
+func reverse(arr *[4]int) *[4]int {
+	for i := range len(arr) / 2 {
+		arr[i], arr[len(arr)-i-1] = arr[len(arr)-i-1], arr[i]
+	}
+
+	return arr
 }
