73b539170d1869e09b4718d1506d372eab2b2e82
Rodion Zherdev <r.zherdev@vigbo.com>
2025-06-26 09:38:21 +0400

    feat: Добавляет функции для работы с json

--- account/account.go
+++ account/account.go
@@ -1,7 +1,10 @@
 package account
 
 import (
+	"demo/password/files"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"math/rand/v2"
 	"net/url"
 	"time"
@@ -12,21 +15,17 @@ import (
 var lettersRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 type Account struct {
-	login    string
-	password string
-	url      string
-}
-
-type AccountWithTimesStamp struct {
-	Account   Account
-	createdAt time.Time
-	updatedAt time.Time
+	Login     string    `json:"login"`
+	Password  string    `json:"password"`
+	URL       string    `json:"url"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (account *Account) OutputPassword() {
-	color.Cyan(account.login)
-	color.Green(account.password)
-	color.Blue(account.url)
+	color.Cyan(account.Login)
+	color.Green(account.Password)
+	color.Blue(account.URL)
 }
 
 func (account *Account) GeneratePassword(length int) {
@@ -35,10 +34,10 @@ func (account *Account) GeneratePassword(length int) {
 		result[i] = lettersRunes[rand.IntN(len(lettersRunes))]
 	}
 
-	account.password = string(result)
+	account.Password = string(result)
 }
 
-func NewAccountWithTimesStamp(login, password, urlString string) (*AccountWithTimesStamp, error) {
+func NewAccount(login, password, urlString string) (*Account, error) {
 
 	if login == "" {
 		return nil, errors.New("login is required")
@@ -51,18 +50,132 @@ func NewAccountWithTimesStamp(login, password, urlString string) (*AccountWithTi
 	}
 
 	newAccount := &Account{
-		login:    login,
-		password: password,
-		url:      urlString,
+		Login:    login,
+		Password: password,
+		URL:      urlString,
 	}
 
 	if len(password) == 0 {
 		newAccount.GeneratePassword(12)
 	}
 
-	return &AccountWithTimesStamp{
-		Account:   *newAccount,
-		createdAt: time.Now(),
-		updatedAt: time.Now(),
+	return &Account{
+		Login:     newAccount.Login,
+		Password:  newAccount.Password,
+		URL:       newAccount.URL,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
 	}, nil
+}
+
+func (account *Account) ToBytes() ([]byte, error) {
+	file, err := json.Marshal(account)
+
+	if err != nil {
+		fmt.Println("Error marshalling account", err)
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func (account *Account) SearchAccount() (*Account, error) {
+
+	fmt.Println("Enter login: ")
+	var login string
+	fmt.Scanln(&login)
+
+	if login == "" {
+		return nil, errors.New("login is required")
+	}
+
+	accountLogin := account.Login
+
+	if accountLogin == login {
+		return account, nil
+	}
+
+	return nil, errors.New("account not found")
+}
+
+func FindAccount() (*Account, error) {
+	fmt.Println("Enter login: ")
+	var inputLogin string
+	fmt.Scanln(&inputLogin)
+
+	if inputLogin == "" {
+		return nil, errors.New("login is required")
+	}
+
+	file, err := files.ReadFile("vault.json")
+
+	if err != nil {
+		color.Red("Error reading file", err.Error())
+		return nil, err
+	}
+
+	var vault Vault
+	err = json.Unmarshal(file, &vault)
+
+	if err != nil {
+		color.Red("Error unmarshalling file", err.Error())
+		return nil, err
+	}
+
+	for _, account := range vault.Accounts {
+		if account.Login == inputLogin {
+			return &account, nil
+		}
+	}
+
+	return nil, errors.New("account not found")
+}
+
+func DeleteAccount() error {
+	fmt.Println("Enter login to delete: ")
+	var inputLogin string
+	fmt.Scanln(&inputLogin)
+
+	if inputLogin == "" {
+		return errors.New("login is required")
+	}
+
+	// Читаем текущий vault
+	file, err := files.ReadFile("vault.json")
+	if err != nil {
+		color.Red("Error reading file", err.Error())
+		return err
+	}
+
+	var vault Vault
+	err = json.Unmarshal(file, &vault)
+	if err != nil {
+		color.Red("Error unmarshalling file", err.Error())
+		return err
+	}
+
+	// Ищем и удаляем аккаунт
+	found := false
+	for i, account := range vault.Accounts {
+		if account.Login == inputLogin {
+			// Удаляем аккаунт из слайса
+			vault.Accounts = append(vault.Accounts[:i], vault.Accounts[i+1:]...)
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return errors.New("account not found")
+	}
+
+	// Сохраняем обновленный vault обратно в файл
+	data, err := vault.ToBytes()
+	if err != nil {
+		color.Red("Error marshalling vault", err.Error())
+		return err
+	}
+
+	files.WriteFile(string(data), "vault.json")
+	return nil
 }
